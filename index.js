f7ce134eae583366bcf45fafe25e03090c3efb79
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2025-05-11 17:58:49 +0200

    Solution of exercise 3.7

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3002
 const MAX = 9999
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 

3f4a9bc7fc65b206e9b76c608c1a8b45e1d8455c
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2025-05-11 17:24:29 +0200

    Solution of exercise 3.2

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ app.get("/api/persons", (request, response) => {
     response.json(persons)
 })
 
+app.get("/info", (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+         <p>${new Date()}</p>`
+    )
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is listening in port ${PORT}`)
 })
